3a02f3ca6e51ea25d42344da3890075d3f58d757
Seynab Nur <seynab.nur@hotmail.com>
2025-11-11 14:46:38 +0100

    testar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import type { omdbResponse } from './models/omdbResponse';
 import './style.css'
 
-
+// seynabs del
 const getMovies = async ()=>{
   const response = await fetch("https://omdbapi.com/?apikey=416ed51a&s=harry");
   const data: omdbResponse =await response.json();
