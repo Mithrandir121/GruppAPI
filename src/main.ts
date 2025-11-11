dae14a99e427dcd016916e755b3759997ab4757d
unknown <araz_95@hotmail.com>
2025-11-11 13:35:58 +0100

    Create html

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,44 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+movies.forEach ((movies => {
+
+  createHtml (movie);
+
+const movieDiv = document.createElement('div');
+const movieTitle = document.createElement('h2');
+const movieYear = document.createElement('p');
+const movieDirector = document.createElement('p');
+const movieGenre = document.createElement('p');
+const movieRating = document.createElement('p');
+const movieDescription = document.createElement('p');
+const moviePoster = document.createElement('img');
+
+movieTitle.textContent = movie.title;
+movieYear.textContent = `Year: ${movie.year}`;
+movieDirector.textContent = `Director: ${movie.director}`;
+movieGenre.textContent = `Genre: ${movie.genre}`;
+movieRating.textContent = `Rating: ${movie.rating}`;
+movieDescription.textContent = movie.description;
+moviePoster.src = movie.poster;
+moviePoster.alt = `${movie.title} Poster`;
+
+movieDiv.appendChild(movieTitle);
+movieDiv.appendChild(movieYear);
+movieDiv.appendChild(movieDirector);
+movieDiv.appendChild(movieGenre);
+movieDiv.appendChild(movieRating);
+movieDiv.appendChild(movieDescription);
+movieDiv.appendChild(moviePoster);
+
+document.body.appendChild(movieDiv);
+  
+});
+
+function createHtml (movie) {
+  const movieElement = document.createElement('div');
+  movieElement.classList.add('movie');
+  movieElement.textContent = movie.title;
+  document.body.appendChild(movieElement);
+}
